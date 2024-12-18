dad2a22f511722ac4c3ae4a4c88d0ee888d98fbd
Solid-Victor <victorcontents@gmail.com>
2024-12-19 00:46:39 +0100

    added the sadcn-ui depedencies

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h2>Real Estate</h2>
+      <Button>Subscribe</Button>
+    </div>
+  )
+}
+
+export default Home
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/shadcn-ui/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
     extend: {
       colors: {
